466132a15a9d0fc15b6da88260f6d36591ffd66a
Wendy Wu <Wendy@Wendys-MacBook-Air.local>
2019-08-13 15:44:41 -0700

    Create package.json file, write HTML form that handles form data using default action

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,26 @@
 
+const express = require('express'); 
+const app = express(); 
+const path = require('path'); 
+const port = 3000; 
+const morgan = require('morgan');
+
+app.use(morgan('combined'))
+app.use(express.static('client'));
+
+
+app.post('/upload_json', (req,res) => {
+    res.send('POST request recieved'); 
+    //respond with another page that contains the form and CSV report 
+    //insert the result of the data processing step into the HTML of the response 
+});
+
+app.get('/upload_json', (req, res) => {
+    // res.sendFile(path.join(__dirname, '/client/index.html'));
+    // res.render('index', {title: 'Hey', message: 'Hello there!'})
+});
+
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!'`)); 
 
